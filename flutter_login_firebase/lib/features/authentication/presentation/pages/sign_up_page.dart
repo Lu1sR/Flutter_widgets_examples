bac1856718df9d811d05d384a0d8dc51c2f348c9
Luis <luishrt1997@gmail.com>
2021-02-16 10:48:56 -0600

    Login with firebase

---
+++ flutter_login_firebase/lib/features/authentication/presentation/pages/sign_up_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_login_firebase/features/authentication/data/repositories/authentication_repository.dart';
+import 'package:flutter_login_firebase/features/authentication/presentation/bloc/sign_up/sign_up_cubit.dart';
+import 'package:flutter_login_firebase/features/authentication/presentation/widgets/sign_up_form.dart';
+
+class SignUpPage extends StatelessWidget {
+  const SignUpPage({Key key}) : super(key: key);
+
+  static Route route() {
+    return MaterialPageRoute<void>(builder: (_) => const SignUpPage());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Sign Up')),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: BlocProvider<SignUpCubit>(
+          create: (_) => SignUpCubit(context.read<AuthenticationRepository>()),
+          child: SignUpForm(),
+        ),
+      ),
+    );
+  }
+}
---
+++ flutter_login_firebase/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_login_firebase/app.dart';
+import 'package:flutter_login_firebase/simply_bloc_observer.dart';
+
+import 'features/authentication/data/repositories/authentication_repository.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  EquatableConfig.stringify = kDebugMode;
+  Bloc.observer = SimpleBlocObserver();
+  runApp(App(authenticationRepository: AuthenticationRepository()));
+}
